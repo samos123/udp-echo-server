f41aa3e939ead39a47d10706eec5f499112de7a4
Sam Stoelinga <sammiestoel@gmail.com>
2019-11-25 09:44:43 -0800

    Fix client script

--- client.sh
+++ client.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-$UDP_SERVER_IP=$1
-#while true
-#do
-#echo -e "sam" | nc -u -q1 $UDP_SERVER_IP 33333 -p 33334
-#done
+set -x
+
+UDP_SERVER_IP="$1"
 
 nping --udp -p 33333 $UDP_SERVER_IP -g 33334 --data-string "test" --rate 50 --count 60000
